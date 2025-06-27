85448174ebecae7cebc4cc4d730e417fa8b893ff
zxcpisya <mamin.papa0@gmail.com>
2025-06-27 18:39:24 +1000

    small fixes

--- test.py
+++ test.py
@@ -15,19 +15,40 @@ def check_users():
 
 def create_card():
 	response = requests.post("http://127.0.0.1:8000/cards/", json={
-		"masked_number": "1234",
-		"name": "моя первая карточка",
-		"owner_id": 1
+		"masked_number": "1333",
+		"name": "моя уже четвертая карточка",
+		"owner_email": "3@gmail.com",
+		"date": "00/01"
 	})
 	print(response.text)
 
 def check_cards():
-	response = requests.get("http://127.0.0.1:8000/users/m_username/cards/")
+	response = requests.get("http://127.0.0.1:8000/users/3@gmail.com/cards/")
 	print(response.text)
 
+def check_dashboard_activity():
+	response = requests.get("http://127.0.0.1:8000/users/3@gmail.com/transactions")
+	print(response.text)
+
+def check_dashboard_activity2():
+	response = requests.get("http://127.0.0.1:8000/users/3%40gmail.com/cards")
+	print(response.text)
+
+def create_transaction():
+	response = requests.post("http://127.0.0.1:8000/transactions", json={
+		"is_income": True,
+		"amount": 100,
+		"category": "Salary",
+		"description": "фуфывфв",
+		"card_id": 5,
+		"icon_res_id": 2
+	})
 
 if __name__ == "__main__":
-	create_user()
-	check_users()
-	create_card()
-	check_cards()
+	create_transaction()
+	# check_users()
+	check_dashboard_activity()
+	# check_dashboard_activity2()
+	#
+	# create_card()
+	# check_cards()
